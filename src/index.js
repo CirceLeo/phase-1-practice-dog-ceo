6dd4d6d15129a79816bd331e7abcab94740fbd21
Calypso Leonard <calypsorleonard@gmail.com>
2022-03-02 18:07:10 -0500

    got final goal working!, but messed w/ style, whoops

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function renderDog(dogLink){
 function renderBreed(breed){
     const newBreed = document.createElement('li')
     newBreed.innerText = breed
+    //newBread.className = 'breeds'
     newBreed.addEventListener("click", event => {
         const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         event.target.style.color = randomColor
@@ -36,9 +37,28 @@ function renderBreed(breed){
     document.querySelector('#dog-breeds').appendChild(newBreed)
 }
 
-function selecctBreeds(event){
+function selecctBreeds(event){ //MAKE IT WORK MORE THAN ONCE -- use style.display = none?
     const firstLetter = event.target.value
-    //console.log(firstLetter)
+    const breeds = document.querySelectorAll('li')
+    //console.log(typeof breeds)
+    /* UNTESTED */
+    //const filteredBreeds = []
+    breeds.forEach(breed => {
+
+        //console.log(typeof breed.innerText.startsWith(firstLetter))
+        if (!breed.innerText.startsWith(firstLetter)){
+            //filteredBreeds.push(breed)
+            breed.style.display = "none"
+        }else{
+            breed.style.display = "block"
+        }
+        //     breed.remove()
+            //console.log("you got in")
+        
+
+        //console.log(filteredBreeds)
+    })
+    //document.querySelector('#dog-breeds').replaceChildren(filteredBreeds) 
 }
 
 document.addEventListener('DOMContentLoaded', init);
